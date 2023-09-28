9a8dd3c16fe3f95b272ae521e7879a3a71676a23
Khoerodin <heykhoerodin@gmail.com>
2023-09-28 07:01:11 +0700

    Pin the Required Packages
    run `bin/importmap pin stimulus_reflex@3.5.0-rc3`

--- config/importmap.rb
+++ config/importmap.rb
@@ -2,6 +2,10 @@
 
 pin "application", preload: true
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
-pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
+pin "@hotwired/stimulus", to: "https://ga.jspm.io/npm:@hotwired/stimulus@3.2.2/dist/stimulus.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
+pin "stimulus_reflex", to: "https://ga.jspm.io/npm:stimulus_reflex@3.5.0-rc3/dist/stimulus_reflex.js"
+pin "@rails/actioncable", to: "https://ga.jspm.io/npm:@rails/actioncable@7.0.8/app/assets/javascripts/actioncable.esm.js"
+pin "cable_ready", to: "https://ga.jspm.io/npm:cable_ready@5.0.1/dist/cable_ready.js"
+pin "morphdom", to: "https://ga.jspm.io/npm:morphdom@2.6.1/dist/morphdom.js"
